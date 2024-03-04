7198748d513ebe7e4f3b91b4a3044a5d6691585b
ClarryWrites <160051721+ClarryWrites@users.noreply.github.com>
2024-03-04 17:43:21 +0000

    Javascript

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-console.log('JavaScript is linked and working!')
+let a = 10
+let b = 7 * a
+console.log(b)
 
 function changeBackgroundColor() {
 document.body.style.backgroundColor = "lightblue"; // Feel free to choose any color
