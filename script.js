391c658a59efe2321161666e5d9f02982d19b4f6
ClarryWrites <160051721+ClarryWrites@users.noreply.github.com>
2024-03-06 17:49:39 +0000

    JavaScript work

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let a = 10
-let b = 7 * a
-console.log(b)
+let a = 10;
+let b = 7 * a;
+console.log (b)
+
+let user = 'John',
+  age = 25,
+  message = 'Hello';
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
 
 function changeBackgroundColor() {
 document.body.style.backgroundColor = "lightblue"; // Feel free to choose any color
