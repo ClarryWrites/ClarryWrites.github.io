f5b5343337bd3b94f7d53bb87a939f72975a9420
ClarryWrites <160051721+ClarryWrites@users.noreply.github.com>
2024-03-13 16:12:21 +0000

    Idk what I did but I am saving

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let a = 10;
 let b = 7 * a;
-console.log (b)
+console.log (9 + 8 / 88)
 
 let user = 'John',
   age = 25,
   message = 'Hello';
 
+let guestList = "Guests:\n * John\n * Pete\n * Mary";
+alert(guestList);
+
 const MAX = 57;
 let actual = MAX - 13;
 let percentage = actual / MAX;
+console.log (percentage)
 
 function changeBackgroundColor() {
 document.body.style.backgroundColor = "lightblue"; // Feel free to choose any color
